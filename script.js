c99ebae1795e1d2583a2f97c53f10aa9b234bd1e
lukavelic <lukavelic@outlook.com>
2022-01-15 13:59:07 +0100

    Initial commit, working calculator

---
+++ script.js
@@ -0,0 +1,118 @@
+let firstValue = 0;
+let numberStorage = [];
+let operatorStorage = [];
+let equalsBoolean = false;
+let equalsValue = 0;
+
+function operate() {
+    if(operatorStorage[0] == ' +') {
+        let runningTotal = numberStorage.reduce(function (total, currentValue) {return total + currentValue})
+        numberStorage.length = 0
+        numberStorage.unshift(runningTotal);
+        console.log(numberStorage)
+    } else if(operatorStorage[0] == ' -') {
+        let runningTotal = numberStorage.reduce(function (total, currentValue) {return total - currentValue})
+        numberStorage.length = 0
+        numberStorage.unshift(runningTotal);
+        console.log(numberStorage)
+    } else if(operatorStorage[0] == ' *') {
+        let runningTotal = numberStorage.reduce(function (total, currentValue) {return total * currentValue})
+        numberStorage.length = 0
+        numberStorage.unshift(runningTotal);
+        console.log(numberStorage)
+    } else if(operatorStorage[0] == ' /') {
+        let runningTotal = numberStorage.reduce(function (total, currentValue) {return total / currentValue})
+        numberStorage.length = 0
+        numberStorage.unshift(runningTotal);
+        console.log(numberStorage)
+    }
+}
+
+function storeValue(event) {
+    firstValue += event.target.getAttribute('data-key');
+}
+
+function pushNumberToArray() {
+    if(firstValue != '') {
+        firstValue = parseFloat(firstValue);
+        numberStorage.push(firstValue);
+        firstValue = '';
+    }
+}
+
+function collectOperator(event) {
+    operatorStorage.push(event.target.id);
+    console.log(operatorStorage)
+
+}
+
+function evaluateEquation() {
+    if(operatorStorage.length > 1) {
+        operate();
+        display.innerText = `${numberStorage[0]}`
+        console.log(equalsValue)
+        operatorStorage.shift();
+    }
+}
+
+// Display updating
+
+function displayUpdateNumber(event) {
+    if(equalsBoolean == false) {
+        display.innerText += event.target.id;
+    } else if(equalsBoolean == true && display.innerText != '' && operatorStorage.length != 1) { 
+        display.innerText = '';      
+        display.innerText += event.target.id;
+        numberStorage.length = 0;
+        return equalsBoolean = false;
+    } else {     
+        display.innerText += event.target.id;
+        numberStorage[0] = equalsValue;
+        console.log(equalsValue)
+        return equalsBoolean = false;
+    }
+}
+
+function displayUpdateOperator(event) {
+    display.innerText += `${event.target.id}\u00A0`
+    console.log(operatorStorage)
+}
+
+function displayUpdateEquals() {
+    display.innerText = numberStorage[0];
+    equalsValue = numberStorage[0];
+    return equalsBoolean = true;
+}
+
+function displayAndStorageClear() {
+    display.innerText = '';
+    firstValue = 0;
+    numberStorage.length = 0;
+    operatorStorage.length = 0;
+}
+
+function equalsResetStorage() {
+    operatorStorage.length = 0;
+}
+
+const display = document.getElementById('result');
+let displayText = display.innerText;
+
+let numbers = document.querySelectorAll('.numberButton');
+numbers.forEach(element => element.addEventListener('click', storeValue));
+numbers.forEach(element => element.addEventListener('click', displayUpdateNumber));
+
+let operators = document.querySelectorAll('.operators');
+operators.forEach(element => element.addEventListener('click', pushNumberToArray));
+operators.forEach(element => element.addEventListener('click', collectOperator));
+operators.forEach(element => element.addEventListener('click', evaluateEquation));
+operators.forEach(element => element.addEventListener('click', displayUpdateOperator));
+
+let equals = document.getElementById('equals');
+equals.addEventListener('click', pushNumberToArray);
+equals.addEventListener('click', operate);
+equals.addEventListener('click', displayUpdateEquals);
+equals.addEventListener('click', equalsResetStorage);
+
+let clear = document.getElementById('clear');
+clear .addEventListener('click', displayAndStorageClear);
